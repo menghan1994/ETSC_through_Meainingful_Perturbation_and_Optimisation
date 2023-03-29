7feb398bb29b07ee36b4d3c7994d11eb4226ed1d
HanMeng_Mac <han.meng.uon@gmail.com>
2023-03-29 21:55:30 +0100

    update the performance of generative model performance

--- Train_GenModel.py
+++ Train_GenModel.py
@@ -1,14 +1,13 @@
-from models.DistributionApproximation.TimeSeriesGen import TimeSeriesGen_GAN
-from Trainer.Trainer_TimeSeriesGen import TimeSeriesGEN_Trainer
-from datasets.MultivariateUEA import *
+from models.DistributionApproximation.TimeSeriesRnnVAEReconstructor import TimeSeriesRNNVAEGen_GAN
+from Trainer.Trainer_TimeSeriesRNNVAEReconstructor import TimeSeriesRNNVAEReconstructorTrainer
+from datasets.copyMultivariateUEA import MultivariateUEA_train, MultivariateUEA_test
 import pandas as pd
-from datasets.mnist import MNIST_train, MNIST_test
 
 def UAEmain():
     
     UEASummary = pd.read_csv('./datasets/Data/MultivariateUEA/Summary.csv')
     
-    for i in [0, 2, 3, 7, 9, 15, 16, 17, 18, 20, 21, 23, 24, 27, 12, 25, 29, 28, 4]:
+    for i in [15]:
         problem = UEASummary.iloc[i]['Problem']
         sequence_length = UEASummary.iloc[i]['SeriesLength']
         MTS_size = [UEASummary.iloc[i]['SeriesLength'], UEASummary.iloc[i]['NumDimensions']]
@@ -18,15 +17,15 @@ def UAEmain():
         num_classes = UEASummary.iloc[i]['NumClasses']
         input_dim = UEASummary.iloc[i]['NumDimensions']
 
-        dataset = problem
-        params = {
-            'input_dim': input_dim,
-            'latent_dim': 32,
-            'sequence_length': sequence_length,
-            'lr':0.001
-        }
+        latent_dim = 128
+        kl_lambda = 0.00001
+        rec_lambda = 1.0
 
-        model = TimeSeriesGen_GAN(params = params, train_dataset=train_dataset, test_dataset=test_dataset, val_dataset=test_dataset)
+        dataset = 'MultivariateUEA'
+        model_label = problem
+
+        print(problem)
+        model = TimeSeriesRNNVAEGen_GAN(latent_dim, kl_lambda, rec_lambda, input_dim = input_dim, sequence_length=sequence_length, train_dataset=train_dataset, test_dataset=test_dataset, val_dataset=test_dataset)
 
         optim_params = {
                     'lr' : 0.0001,
@@ -34,48 +33,15 @@ def UAEmain():
                 }
 
         logs_params = {
-            'log_dir' : f'logs/{dataset}/{dataset}'
+            'log_dir' : f'runs/GEN_tunning-200/{dataset}/{model_label}'
         }
 
         modelsave_params = {
-            'model_save_dir': f'TrainedModel/Transformer_Gen/{dataset}/'
+            'model_save_dir': f'TrainedModel/GEN_tunning-200/{dataset}/{model_label}'
         }
 
-        trainer = TimeSeriesGEN_Trainer(optim_params=optim_params, logs_params=logs_params, modelsave_params=modelsave_params)
+        trainer = TimeSeriesRNNVAEReconstructorTrainer(optim_params=optim_params, logs_params=logs_params, modelsave_params=modelsave_params)
         trainer.fit(model)
-    
-
-def MNIST_main():
-    
-    
-    train_dataset, test_dataset = MNIST_train(), MNIST_test()
-      
-    dataset = 'MNIST'
-    params = {
-        'input_dim': 28,
-        'latent_dim': 32,
-        'sequence_length': 28,
-        'lr':0.001
-    }
-
-    model = TimeSeriesGen_GAN(params = params, train_dataset=train_dataset, test_dataset=test_dataset, val_dataset=test_dataset)
-
-    optim_params = {
-                'lr' : 0.0001,
-                'max_epoches' : 1000,
-            }
-
-    logs_params = {
-        'log_dir' : f'logs/{dataset}/{dataset}'
-    }
-
-    modelsave_params = {
-        'model_save_dir': f'TrainedModel/Transformer_Gen/{dataset}/'
-    }
-
-    trainer = TimeSeriesGEN_Trainer(optim_params=optim_params, logs_params=logs_params, modelsave_params=modelsave_params)
-    trainer.fit(model)
-
 
 if __name__ == '__main__':
-    MNIST_main()
+    UAEmain()
